26ea9416705efb6b746364d2be94d207729d3f3e
Tim Cooper <timothy.cooper@shea.ca>
2015-08-06 23:27:13 -0400

    Working To Do app

--- simple-todos.js
+++ simple-todos.js
@@ -1,23 +1,49 @@
+Tasks = new Mongo.Collection("tasks");
+
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
+    // This code only runs on the client
+    Template.body.helpers({
+        tasks: function () {
+            return Tasks.find({});
+        }
+    });
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
+    Template.body.events({
+        "submit .new-task": function (event) {
+            // Prevent default browser form submit
+            event.preventDefault();
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
-}
+            // Get value from form element
+            var text = event.target.text.value;
 
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
+            // Insert a task into the collection
+            Tasks.insert({
+                text: text,
+                createdAt: new Date() // current time
+            });
+
+            // Clear form
+            event.target.text.value = "";
+        }
+    });
+
+    Template.task.events({
+        "click .toggle-checked": function () {
+            // Set the checked property to the opposite of its current value
+            Tasks.update(this._id, {
+                $set: {checked: ! this.checked}
+            });
+        },
+        "click .delete": function () {
+            Tasks.remove(this._id);
+        }
+    });
+
+    // This code only runs on the client
+    Template.body.helpers({
+        tasks: function () {
+            // Show newest tasks at the top
+            return Tasks.find({}, {sort: {createdAt: -1}});
+        }
+    });
 }
